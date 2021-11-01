1f4eca7f47d062d73cd7efc0dfa85fdfd1ef20a9
Sumit Chavhan <srchavhan29@gmail.com>
2021-11-01 23:34:20 +0530

    coding challenge 4

--- challenge1/index.js
+++ challenge1/index.js
@@ -40,25 +40,61 @@
 //     console.log("average score of koalas is more so koalas won the match")
 // }
 
-// bonus 1
+// // bonus 1
 
-const dolPhinsOne = 98;
-const dolPhinsTwo = 110;
-const dolPhinsThree = 100;
+// const dolPhinsOne = 98;
+// const dolPhinsTwo = 110;
+// const dolPhinsThree = 100;
 
-const koalasOne = 98;
-const koalasTwo = 100;
-const koalasThree = 110;
+// const koalasOne = 98;
+// const koalasTwo = 100;
+// const koalasThree = 110;
 
-const avgDolphins = (dolPhinsOne + dolPhinsTwo + dolPhinsThree)/3 ;
-const avgKoalas = (koalasOne + koalasTwo + koalasThree)/3 ;
-console.log(avgKoalas, avgDolphins);
+// const avgDolphins = (dolPhinsOne + dolPhinsTwo + dolPhinsThree)/3 ;
+// const avgKoalas = (koalasOne + koalasTwo + koalasThree)/3 ;
+// console.log(avgKoalas, avgDolphins);
 
 
-if(avgDolphins > avgKoalas && avgDolphins >=100){
-    console.log("average score of dolphins is more so dolphin won the match")
-}else if(avgKoalas > avgDolphins && avgKoalas >=100){
-    console.log("average score of koalas is greater so koalas won the match")
-}else if (avgDolphins === avgKoalas && avgDolphins>=100 && avgKoalas>=100){
-    console.log("match draw")
-}
+// if(avgDolphins > avgKoalas && avgDolphins >=100){
+//     console.log("average score of dolphins is more so dolphin won the match")
+// }else if(avgKoalas > avgDolphins && avgKoalas >=100){
+//     console.log("average score of koalas is greater so koalas won the match")
+// }else if (avgDolphins === avgKoalas && avgDolphins>=100 && avgKoalas>=100){
+//     console.log("match draw")
+// }else{
+//     console.log("No One won the match")
+// }
+
+//switch statement
+
+// const day = 'wednesday';
+// switch (day) {
+//     case 'monday':
+//         console.log('today is a good day')
+//     break;
+//     case 'tuesday':
+//         console.log('today you have to go work')
+//     break;
+//     case 'wednesday':
+//         console.log('today you have to got to gym')
+//     break;
+//     default:
+//         console.log('please enter valid day')    
+// }
+
+// coding challenge 4
+
+// const billAmount= 300;
+// let tip;
+// if(billAmount<=300 && billAmount>=50){
+//     tip = billAmount*0.15;
+// console.log(`billamount is ${billAmount} and tip given will be ${tip}`);
+// }else{
+//     tip = billAmount*0.20;
+//     console.log(`billamount is ${billAmount} and tip given will be ${tip}`);
+// }
+                // or
+    
+// const billAmount = 400;
+// const tip= billAmount<=300 && billAmount>=50 ? billAmount*0.15 : billAmount*0.20;
+// console.log(`billamount is ${billAmount} and tip given will be ${tip}`);
